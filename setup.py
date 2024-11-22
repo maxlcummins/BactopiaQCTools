c1514013095668c0191942fec4b1d93bdcb35c24
maxlcummins <maxlcummins@gmail.com>
2024-11-22 12:22:44 +0930

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='bactQC',
+    version='0.1',
+    py_modules=['bactQC'],
+    author='Max Cummins',
+    author_email='max.l.cummins@gmail.com',
+    description='A package for quality control of bactopia output data',
+    url='https://github.com/yourusername/bactQC',  # Optional
+)
